89c1033ca15f67b069acec2818d1829a387ccafc
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2023-11-02 20:03:37 -0400

    used the div id container to render the cards from the API using innerHTML and JS html for the images.

--- app.js
+++ app.js
@@ -14,5 +14,11 @@ document.getElementById("new-deck").addEventListener("click", handleClick);
 document.getElementById("draw").addEventListener("click", () => {
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            document.getElementById("cards-display").innerHTML = `
+            <img src =${data.cards[0].image} />
+            <img src =${data.cards[1].image} />
+            `
+        })
     })
